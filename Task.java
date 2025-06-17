29dbcf9db93a10380449aa400817342e061bf766
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-06-17 14:13:33 -0230

    Updating task.java and tasklist.java

--- Task.java
+++ Task.java
@@ -1,3 +1,28 @@
 public class Task {
-    
+    private String description;
+    private Boolean isComplete;
+    Task next;
+
+    public Task(String description) {
+        this.description = description;
+        this.isComplete = false;
+        this.next = null;
+    }
+
+    public void markComplete() {
+        this.isComplete = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    @Override
+    public String toString() {
+        return description + ": " + (isComplete ? "Complete" : "Incomplete");
+    }
 }
--- TaskList.java
+++ TaskList.java
@@ -1,3 +1,42 @@
 public class TaskList {
-    
+    private Task head;
+
+    public TaskList() {
+        this.head = null;
+    }
+
+    // Adding a new task
+    public void addTask(String description) {
+        Task newTask = new Task(description);
+        if (head == null) {
+            head = newTask;
+        } else {
+            Task current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newTask;
+        }
+    }
+
+    // Marking a task as completed
+    public boolean markAsComplete(String description) {
+        Task current = head;
+        while (current != null) {
+            if (current.getDescription().equals(description)) {
+                current.markComplete();
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    // Displaying tasks and completion status
+    public void printTasks() {
+        Task current = head;
+        while (current != null) {
+            System.out.println(current.getDescription() + ": " + (current.isComplete() ? "Complete" : "Incomplete"));
+        }
+    }
 }
